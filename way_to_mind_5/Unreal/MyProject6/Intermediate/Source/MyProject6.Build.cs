0bff24d5951b9dde0c6722a24535c1b143e8863f
Caro <carolin-olbrich@gmx.de>
2025-08-04 16:18:39 +0200

    merge

---
+++ way_to_mind_5/Unreal/MyProject6/Intermediate/Source/MyProject6.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class MyProject6 : ModuleRules
+{
+	public MyProject6(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
